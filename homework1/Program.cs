f9b2a83e3a838cb3a2620ea0de9e74e22442240b
RobertGru <robertgrutza@gmail.com>
2015-04-07 23:38:31 -0500

    first commit

---
+++ homework1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // student variables
+            string strFirstName = "Joe";
+            string strLastName = "Learner";
+            DateTime birthDate = new DateTime(2000, 1, 20);
+            string strAddress1 = "123 Maple Ln";
+            string strAddress2 ="";
+            string strCity = "Chicago";
+            string strState = "IL";
+            int intZip = 60614;
+            string strCountry = "USA";
+
+            // professor variables
+            string strProfFirst = "Jack";
+            string strProfLast = "Smarty";
+            string strSchool = "Computer Science";
+        }
+    }
+}
